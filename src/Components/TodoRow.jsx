d286337360f935af4b4199da9c3c8b8203aebbb5
Ranjeet Singh <ranjeetsinghfattu@gmail.com>
2023-05-19 13:22:44 +0530

    added functionality of onhover button shows otherwise it  hides

--- src/Components/TodoRow.jsx
+++ src/Components/TodoRow.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
 import Checkbox from './Checkbox'
 import Button from './Button'
 import './TodoRow.css'
+import React, { useState } from 'react'
+
+
 function TodoRow() {
+const [ishover,sethover]=useState(false);
+const handleMouseEnter=()=>{
+  sethover(true);
+}
+const handleMouseLeave=()=>{
+  sethover(false);
+}
   return (
     <div>
-      <div className="wrapper">
+      <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className="wrapper">
         <Checkbox label='Random text' />
-        <Button size='small' content='X'/>
+        {ishover && <Button  size='small' content='X'/>}
       </div>
     </div>
   )
